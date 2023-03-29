610e0f532cfea7c76e10bec7e648793bf6fc4c07
zeitounmax <mthiry1@gmail.com>
2023-03-29 10:55:39 +0200

    commit 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 function PokemonCard () {
+    const pokemon = pokemonList[0];
+    const {name, imgSrc} = pokemon;
     return (
-        <div className="bulbasaur">
-      <figure>
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-    <figcaption>bulbasaur</figcaption>
+        <>
+      <figure className="card">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="bulbasaur"
+        className="card-img"
+      />
+         <figcaption>bulbasaur</figcaption>
 </figure>
     
-        </div>
+        </>
+
     )
 }
 
