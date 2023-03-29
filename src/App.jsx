3c91b32e60c87a9e008c23ee14d85e4015ceef06
zeitounmax <mthiry1@gmail.com>
2023-03-29 10:07:58 +0200

    bulbasaur

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-       <h1>Pokémon Card</h1>  
-       <p>This is an exercice from Odyseey Wild Code School </p>
-      </div>
-      
-      <p className="read-the-docs">
-        Go to Odyseey Website for More information
-      </p>
+      <PokemonCard/>
     </div>
   )
 }
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function PokemonCard () {
+    return (
+        <div className="bulbasaur">
+      <figure>
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+    <figcaption>bulbasaur</figcaption>
+</figure>
+    
+        </div>
+    )
+}
+
+export default PokemonCard;
