a5ab1277b97ef12b8996786c3977b4a5fa78b1b9
zeitounmax <mthiry1@gmail.com>
2023-03-30 09:21:09 +0200

    SecondCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PokemonCard from './components/PokemonCard'
+
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <PokemonCard/>
+    <div>
+      <PokemonCard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,33 +1,30 @@
 import React from "react";
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-
-function PokemonCard () {
-    const pokemon = pokemonList[0];
-    const {name, imgSrc} = pokemon;
-    return (
-        <>
-      <figure className="card">
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-        className="card-img"
-      />
-         <figcaption>bulbasaur</figcaption>
-</figure>
-    
-        </>
-
-    )
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+  let imageElement = null;
+  if (pokemon.imgSrc) {
+    imageElement = <img src={pokemon.imgSrc} alt={pokemon.name} />;
+  } else {
+    imageElement = <p>???</p>;
+  }
+  return (
+    <figure>
+      {imageElement}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
